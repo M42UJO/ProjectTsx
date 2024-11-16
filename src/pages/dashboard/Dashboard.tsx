5f4b03138be4ec24e55d5162611bb8f9b7b4cc09
Gumpwsw <wasawatsutthitham@gmail.com>
2024-11-16 12:37:29 +0700

    up

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,16 +1,13 @@
-import DashboardHeader from "./components/DashboardHeader"
-import DashboarfSestion from "./components/DashboarfSestion"
-
+import DashboardHeader from "./components/DashboardHeader";
+import DashboarfSestion from "./components/DashboarfSestion";
 
 function Dashboard() {
   return (
-    <div className="flex h-screen p-4">
-    <main className="flex-grow p-6 rounded-md shadow-lg bg-[#F5F6FB]">
-        <DashboardHeader/>
-        <DashboarfSestion/>
-    </main>
-  </div>
-  )
+    <div>
+      <DashboardHeader />
+      <DashboarfSestion />
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
--- src/pages/setting/Setting.tsx
+++ src/pages/setting/Setting.tsx
@@ -2,22 +2,16 @@ import { useState } from "react";
 import Header from "./components/Header";
 import SettingList from "./components/SettingList";
 import SettingEdit from "./components/SettingEdit";
-;
-
-export default function EditSetting() {
+export default function Setting() {
   const [activeTab, setActiveTab] = useState<string>("รายการ");
 
   return (
-    <div className="flex h-screen p-4">
-      <main className="flex-grow p-0 rounded-md shadow-lg bg-[#F5F6FB]">
-        <div className="p-0">
-          {/* Header */}
-          <Header activeTab={activeTab} setActiveTab={setActiveTab} />
+    <div >
+      {/* Header */}
+      <Header activeTab={activeTab} setActiveTab={setActiveTab} />
 
-          {/* Content Section */}
-          {activeTab === "รายการ" ? <SettingList/> : <SettingEdit />}
-        </div>
-      </main>
+      {/* Content Section */}
+      {activeTab === "รายการ" ? <SettingList /> : <SettingEdit />}
     </div>
   );
 }
